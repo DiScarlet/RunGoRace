3783d14741b18d9674ff64affaac5eee313f1f66
DiScarlet <antoniukdin@gmail.com>
2025-12-30 00:05:00 +0100

    Finished krab diaogue

--- scripts/krab.gd
+++ scripts/krab.gd
@@ -10,21 +10,25 @@ func _ready() -> void:
 	GameState.krab_hurt.connect(_on_hurt)
 
 func  _on_player_entered(body):
+	print("entered kr")
 	if body is CharacterBody2D:
 		var dialogue_resource = load("res://dialogues/dialogueKrab.dialogue")
 		DialogueManager.show_dialogue_balloon(dialogue_resource, "start")
 
 func _on_attack():
+	print("attacked by kr")
 	animated_sprite.play("attack")
 	await animated_sprite.animation_finished
 	animated_sprite.play("default")
 	
 func _on_death():
+	print("dead kr")
 	animated_sprite.play("death")
 	await animated_sprite.animation_finished
 	animated_sprite.play("default")
 	
 func _on_hurt():
+	print("hurt kr")
 	animated_sprite.play("hurt")
 	await animated_sprite.animation_finished
 	animated_sprite.play("default")
