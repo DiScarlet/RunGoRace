a86831e1d999b933f90fe1db23f46309152a7519
DiScarlet <antoniukdin@gmail.com>
2026-01-02 13:42:18 +0100

    Fixed Healer dialogue

--- scripts/healer.gd
+++ scripts/healer.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+var in_dialogue = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("Healer entered")
-	if body is CharacterBody2D:
+	if body is CharacterBody2D and not in_dialogue:
+		in_dialogue = true
 		var dialogue_resource = load("res://dialogues/dialogueHealer.dialogue")
 		DialogueManager.show_dialogue_balloon(dialogue_resource, "samurai_start")
